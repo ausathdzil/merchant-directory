d1b06c0fdf94e0571f060c526c8bc50645cefaa5
Ausath Ikram <ausathdzil@gmail.com>
2025-10-26 15:32:07 +0700

    Simplify proxy and intl default

--- web/src/proxy.ts
+++ web/src/proxy.ts
@@ -1,32 +1,10 @@
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { locales } from '../global';
-import { setLocale } from './lib/actions/locale';
 import { getUser } from './lib/data/users';
-
-function getLocale(req: NextRequest) {
-  const headers = {
-    'accept-language': req.headers.get('accept-language') || 'en-US,en;q=0.5',
-  };
-  const languages = new Negotiator({ headers }).languages();
-  const defaultLocale = 'id';
-
-  return match(languages, locales, defaultLocale);
-}
 
 const authRoute = ['/login', '/register'];
 
 export default async function proxy(req: NextRequest) {
-  const locale = getLocale(req) as (typeof locales)[number];
-
-  const cookieStore = await cookies();
-  if (!cookieStore.get('locale')) {
-    await setLocale(locale);
-  }
-
   const path = req.nextUrl.pathname;
   const isAuthRoute = authRoute.includes(path);
 
